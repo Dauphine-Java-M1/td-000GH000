4ec2927b248f4ce82c07f13ee08a54f513cd2cde
xula18 <55983249+000GH000@users.noreply.github.com>
2019-12-02 15:14:29 +0100

    Ajout de la méthode panel() dans Panel.java + test du fichier PanelTest.java réussi

--- iterables/src/main/java/fr/dauphine/ja/xulaurent/iterables/Panel.java
+++ iterables/src/main/java/fr/dauphine/ja/xulaurent/iterables/Panel.java
@@ -1,12 +1,14 @@
 package fr.dauphine.ja.xulaurent.iterables;
 
+import java.util.AbstractList;
 import java.util.Iterator;
+import java.util.List;
 
-public class Panel implements Iterable<Integer>{
-	
+public class Panel extends AbstractList<Integer> implements Iterable<Integer> {
+
 	private int deb;
 	private int fin;
-	
+
 	public Panel(int deb, int fin) {
 		this.deb = deb;
 		this.fin = fin;
@@ -15,22 +17,33 @@ public class Panel implements Iterable<Integer>{
 		Iterator<Integer> it = panel1(1, 5);
 
 		for (;it.hasNext();) {
-			System.out.println(it.next());
+			System.out.print(it.next() + " ");
 		}
-		
+		System.out.println();
+
 		for (Integer i : panel2(1, 5)) {
-			System.out.println(i);
+			System.out.print(i + " ");
 		}
+		System.out.println();
+
+		List<Integer> l = panel(3,6);
+		for(int i: l) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		System.out.println(l.get(1));
+		System.out.println(Panel.panel(0, 2).size());
 	}
-	
-//	public static Iterator<Integer> panel1(int deb, int fin) {
-//		return new IntegerIterator(deb, fin);
-//	}
+
+	//	public static Iterator<Integer> panel1(int deb, int fin) {
+	//		return new IntegerIterator(deb, fin);
+	//	}
 
 	public static Iterator<Integer> panel1(final int deb, final int fin) {
 		Iterator<Integer> it = new Iterator<Integer>() {
 			int i = deb;
-			
+
 			public boolean hasNext() {
 				if (i <= fin) {
 					return true;
@@ -49,11 +62,10 @@ public class Panel implements Iterable<Integer>{
 				}
 			}
 		};
-		
-		return it;
 
+		return it;
 	}
-	
+
 	public static Panel panel2(int deb, int fin) {
 		return new Panel(deb, fin);
 	}
@@ -61,5 +73,33 @@ public class Panel implements Iterable<Integer>{
 	public Iterator<Integer> iterator() {
 		return panel1(deb, fin);
 	}
-	
+
+	public static List<Integer> panel(int deb, int fin) throws IllegalArgumentException{
+		if (deb > fin) {
+			throw new IllegalArgumentException();
+		} 
+		return new Panel(deb, fin);
+	}
+
+	@Override
+	public Integer get(int i) {
+		Iterator<Integer> it = panel2(deb, fin).iterator();
+		while (i > 0) {
+			it.next();
+			i--;
+		}
+		return it.next();
+	}
+
+	@Override
+	public int size() {
+		int size = 0;
+		Iterator<Integer> it = Panel.panel2(deb, fin).iterator();
+		while (it.hasNext()) {
+			it.next();
+			size++;
+		}
+		return size;
+	}
+
 }
